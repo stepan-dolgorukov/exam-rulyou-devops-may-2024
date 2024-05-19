7a50c4cd2842ce6c1baac5e24813ebd85d481e74
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-05-19 14:37:14 +0500

    Определение и применение переменной «catalog_certificates»

--- copy-certificate.bash
+++ copy-certificate.bash
@@ -2,6 +2,7 @@
 
 host_certbot='192.168.57.5'
 pattern_certificate='*.rulyou.ru.pem'
+catalog_certificates='/etc/ssl/certs'
 
 targets=(
   '192.168.57.5'
@@ -9,7 +10,7 @@ targets=(
 
 mkdir certificates
 
-for certificate in $(ssh root@${host_certbot} find /etc/ssl -name "${pattern_certificate}");
+for certificate in $(ssh root@${host_certbot} find ${catalog_certificates} -name "${pattern_certificate}");
 do
   rsync root@${host_certbot}:"${certificate}" ./certificates/
 done
@@ -22,8 +23,8 @@ do
 
   for container in ${containers}; do
 
-    ssh root@"${host}" docker exec "${container}" mkdir --parent /etc/ssl
-    ssh root@${host} docker cp ${container}:/etc/ssl/ certificates_${container}
+    ssh root@"${host}" docker exec "${container}" mkdir --parent ${catalog_certificates}
+    ssh root@${host} docker cp ${container}:${catalog_certificates} certificates_${container}
 
     for certificate in $(ssh root@"${host}" find '~/certificates' -type f);
     do
@@ -33,7 +34,7 @@ do
 
       if [ ${answer} = '0' ]; then
         echo "${host}:${container}:${certificate}"
-        ssh root@"${host}" docker cp "${certificate}" "${container}":/etc/ssl/
+        ssh root@"${host}" docker cp "${certificate}" "${container}":${catalog_certificates}
       fi
     done
 
