1884c42676f0c947a51490f12a8316f56b824200
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-05-19 14:57:56 +0500

    Экранирование двойными кавычками

--- copy-certificates.bash
+++ copy-certificates.bash
@@ -24,25 +24,25 @@ do
   for container in ${containers}; do
 
     ssh root@"${host}" docker exec "${container}" mkdir --parent ${catalog_certificates}
-    ssh root@${host} docker cp ${container}:${catalog_certificates} certificates_${container}
+    ssh root@"${host}" docker cp "${container}":"${catalog_certificates}" certificates_"${container}"
 
     for certificate in $(ssh root@"${host}" find '~/certificates' -type f);
     do
       certificate_base=$(basename ${certificate})
 
-      answer=$(ssh root@${host} diff \
-        '~/certificates'/${certificate_base} \
-        '~/certificates_'${container}/${certificate_base} \
+      answer=$(ssh root@"${host}" diff \
+        '~/certificates'/"${certificate_base}" \
+        '~/certificates_'"${container}/${certificate_base}" \
         2>/dev/null >/dev/null && \
         echo '1' || echo '0')
 
-      if [ ${answer} = '0' ]; then
+      if [ "${answer}" = '0' ]; then
         echo "${host}:${container}:${certificate_base}"
         ssh root@"${host}" docker cp "${certificate}" "${container}":${catalog_certificates}
       fi
     done
 
-    ssh root@${host} rm -r certificates_${container}
+    ssh root@"${host}" rm -r certificates_"${container}"
   done
 
   ssh root@"${host}" rm --recursive '~/certificates'
